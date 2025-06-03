b1efe5ae64e53b61b8e2abff74661ec925836c4c
Ion Gireada <shytiger@yahoo.com>
2025-06-03 09:11:53 +0300

    update logUtils.js

--- src/utils/logUtils.js
+++ src/utils/logUtils.js
@@ -1,36 +1,46 @@
-const winston = require('winston');
-const colors = require('ansi-colors');
+const winston = require("winston");
+const colors = require("ansi-colors");
 
-const logFilePath = process.env.LOG_FILE_PATH || 'transpile.log';
+const logFilePath = process.env.LOG_FILE_PATH || "transpile.log";
 
 const logger = winston.createLogger({
-  level: 'info',
+  level: "info",
   format: winston.format.combine(
-    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    winston.format.printf(({ timestamp, level, message }) => `[${timestamp}] [${level.toUpperCase()}] ${message}`)
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(
+      ({ timestamp, level, message }) =>
+        `[${timestamp}] [${level.toUpperCase()}] ${message}`
+    )
   ),
-  transports: [new winston.transports.File({
+  transports: [
+    new winston.transports.File({
       filename: logFilePath,
       maxsize: 5242880, // 5MB
       maxFiles: 5,
       tailable: true,
       zippedArchive: true,
-      handleExceptions: true })]
+      handleExceptions: true,
+    }),
+  ],
 });
 
 const levelColors = {
   error: colors.red,
   warn: colors.yellow,
   info: colors.green,
-  debug: colors.blue
+  debug: colors.blue,
 };
 
-const validLogLevels = ['error', 'warn', 'info', 'debug'];
+const validLogLevels = ["error", "warn", "info", "debug"];
 const logMessage = (level, message, isSilent) => {
   if (!validLogLevels.includes(level)) {
-    throw new Error(`Invalid log level: ${level}. Must be one of: ${validLogLevels.join(', ')}`);
+    throw new Error(
+      `Invalid log level: ${level}. Must be one of: ${validLogLevels.join(
+        ", "
+      )}`
+    );
   }
   logger.log({ level, message });
-}
+};
 
 module.exports = { logMessage };
